ca1da521a59aa127e3fd44bb8daa10a75570b6ef
GlazyevaValentina <116272374+GlazyevaValentina@users.noreply.github.com>
2022-11-05 13:54:09 +0200

    Add files via upload

---
+++ To_Lesson8/main.py
@@ -0,0 +1,9 @@
+
+def describe_pet(animal_type,pet_name):
+    # Information about pet
+    print(f"\nI have a {animal_type}")
+    print(f"My {animal_type}'s name is {pet_name.title()}")
+#describe_pet("cat", "Vasya")
+describe_pet("cat", "Semen")
+print("I love all my pets")
+print("The end")
